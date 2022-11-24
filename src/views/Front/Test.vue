30ca02d001c5548c7ab778b6864fa1cfe40e7931
sam <84328193+highdanny11@users.noreply.github.com>
2022-11-24 21:24:25 +0800

    測試 線上環境

---
+++ src/views/Front/Test.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mt-10">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-6">
+        <input type="date">
+      </div>
+    </div>
+  </div>
+</template>
